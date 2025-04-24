1c1aee9dad8e3516ab9bfc2a291e724c7494ce13
nabs-dev <nabihakashif631@gmail.com>
2025-04-24 17:28:32 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli("localhost", "u8gr0sjr9p4p4", "9yxuqyo3mt85", "dbe9bgbu9pu7b6");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
